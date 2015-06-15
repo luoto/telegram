dfa6f61ffb75fc20d22be6e083de3f72b3b6c5e2
Tony Luo <luo.tony37@gmail.com>
2015-06-15 16:59:26 -0700

    Add user and tweet models

--- db/index.js
+++ db/index.js
@@ -1,10 +1,15 @@
 var mongoose = require('mongoose');
 var config = require('../config');
+var user = require('./schemas/user');
+var tweet = require('./schemas/tweet');
 
 var connection = mongoose.createConnection(
   config.get('database:host'),
   config.get('database:name'),
   config.get('database:port')
 );
+
+connection.model('User', user);
+connection.model('Tweet', user);
 
 module.exports = connection;
