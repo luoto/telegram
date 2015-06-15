964eb0d16a94c181589bb47043128f8a5a9fe7d3
Tony Luo <luo.tony37@gmail.com>
2015-06-15 14:55:33 -0700

    Refactor config file

--- config/index.js
+++ config/index.js
@@ -1,21 +1,9 @@
-var fs = require('fs');
 var nconf = require('nconf');
 var path = require('path');
 
 nconf.env();
 
-if (process.env.NODE_ENV === 'prod') {
-  var configPath = path.join(__dirname, 'config-prod.json');
-}
-
-if (process.env.NODE_ENV === 'dev') {
-  var configPath = path.join(__dirname, 'config-dev.json');
-}
-
-if (process.env.NODE_ENV === 'test') {
-  var configPath = path.join(__dirname, 'config-test.json');
-}
-
-nconf.file(configPath);
+var configFile = 'config-' + process.env.NODE_ENV + '.json';
+nconf.file(path.join(__dirname, configFile));
 
 module.exports = nconf;
